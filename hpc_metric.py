9933d296921c6a6096ecb19dd73bf33478ac20d9
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2020-07-30 16:05:04 +0200

    foo

--- hpc_metric.py
+++ hpc_metric.py
@@ -10,7 +10,6 @@ from himlarcli.parser import Parser
 from himlarcli.printer import Printer
 from himlarcli import utils as himutils
 from datetime import date, timedelta, datetime
-from time import mktime
 from collections import defaultdict
 
 parser = Parser()
@@ -68,7 +67,8 @@ def action_get_cpu_util():
                                                        stop=stop)
         for i in range (len(cpu_util)):
             try:
-                cpu_util[i]=time.mktime(cpu_util[i].timetuple())
+                #cpu_util[i]=time.mktime(cpu_util[i].timetuple())
+                cpu_util[i]=cpu_util[i].timestamp()
             except ValueError:
                 next
 
