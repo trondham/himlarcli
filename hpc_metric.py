30b632cc32a8f231e4d1e6a5abb6921765e96b40
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2020-07-30 18:21:47 +0200

    foo

--- hpc_metric.py
+++ hpc_metric.py
@@ -24,27 +24,6 @@ logger = kc.get_logger()
 nc = Nova(options.config, debug=options.debug, log=logger)
 nc.set_dry_run(options.dry_run)
 
-def action_instance():
-    start = himutils.get_date(options.start, date.today() - timedelta(days=1))
-    stop = himutils.get_date(options.end, date.today() + timedelta(days=1))
-    gc = Gnocchi(options.config, debug=options.debug, log=logger)
-    instance = nc.get_instance(options.instance)
-    resources = gc.get_resource(resource_type='instance', resource_id=instance.id)
-    metrics = resources['metrics']
-    del resources['metrics']
-    printer.output_dict({'header': 'instance metadata'})
-    printer.output_dict(resources)
-    printer.output_dict({'header': 'instance metrics'})
-    output = defaultdict(int)
-    for k, v in metrics.iteritems():
-        measurement = gc.get_client().metric.get_measures(metric=v,
-                                                          aggregation='max',
-                                                          start=start,
-                                                          stop=stop)
-        if measurement:
-            output[k] += measurement[0][2]
-    printer.output_dict(output)
-
 def action_list_metrics():
     gc = Gnocchi(options.config, debug=options.debug, log=logger)
     res = gc.list_metrics()
@@ -70,13 +49,9 @@ def action_get_cpu_util():
                                                        stop=stop)
         timeseries = {}
         for i in range (len(cpu_util)):
+            if len(cpu_util[i]) < 3:
+                next
             timeseries[int(time.mktime(cpu_util[i][0].timetuple()))] = cpu_util[i][2]
-#            cpu_util[i][0]=time.mktime(cpu_util[i][0].timetuple())
-#            try:
-#                cpu_util[i]=time.mktime(cpu_util[i].timetuple())
-#            except ValueError:
-#                next
-
         #print timeseries
         pprint.pprint(timeseries, width=1)
         
