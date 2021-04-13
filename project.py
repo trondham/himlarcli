869973be315ca015d32e7ebe4ea4b2b256611d4e
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2021-04-13 13:50:57 +0200

    foo

--- project.py
+++ project.py
@@ -204,8 +204,8 @@ def action_trond():
         print "---------------------------------------------------------------------------"
         print "  ID:   %s" % project.id
         print "  Type: %s" % project_type
-        print "  Description: "
-        print "    \n".join(textwrap.wrap(project.description, 50))
+        "print "  Description: "
+        print "\n    ".join(textwrap.wrap("  Description: " + project.description, 50))
         #print textwrap.fill(project.description, 50).indent(4)
         print
         count += 1
