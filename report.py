f87bbf34fbc019a7f8ce4a093e54a702e412ebae
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2021-06-11 20:43:09 +0200

    add more stuff in report

--- report.py
+++ report.py
@@ -1,20 +1,14 @@
-#!/usr/bin/env python
+!/usr/bin/env python
 from himlarcli.keystone import Keystone
 from himlarcli.nova import Nova
 from himlarcli.cinder import Cinder
-from himlarcli.neutron import Neutron
+from himlarcli.designate import Designate
+from himlarcli.glance import Glance
 from himlarcli.parser import Parser
 from himlarcli.printer import Printer
-from himlarcli.mail import Mail
 from himlarcli import utils as himutils
-from datetime import datetime
-from email.mime.text import MIMEText
-import re
-
-import textwrap
-from himlarcli.glance import Glance
-from himlarcli import utils as utils
 from prettytable import PrettyTable
+import re
 
 himutils.is_virtual_env()
 
@@ -22,16 +16,12 @@ parser = Parser()
 parser.set_autocomplete(True)
 options = parser.parse_args()
 printer = Printer(options.format)
-project_msg_file = 'notify/project_created.txt'
-project_hpc_msg_file = 'notify/project_created_hpc.txt'
-access_msg_file = 'notify/access_granted_rt.txt'
-access_user_msg_file = 'notify/access_granted_user.txt'
 
 ksclient = Keystone(options.config, debug=options.debug)
 ksclient.set_dry_run(options.dry_run)
 ksclient.set_domain(options.domain)
 logger = ksclient.get_logger()
-#novaclient = Nova(options.config, debug=options.debug, log=logger)
+
 if hasattr(options, 'region'):
     regions = ksclient.find_regions(region_name=options.region)
 else:
@@ -40,109 +30,114 @@ else:
 if not regions:
     himutils.sys_error('no regions found with this name!')
 
-def action_xlist():
-    search_filter = dict()
-    if options.filter and options.filter != 'all':
-        search_filter['type'] = options.filter
-    projects = ksclient.get_projects(**search_filter)
-    instances = dict()
-    count = 0
-    for project in projects:
-        project_type = project.type if hasattr(project, 'type') else '(unknown)'
-        project_admin = project.admin if hasattr(project, 'admin') else '(unknown)'
-        roles = ksclient.list_roles(project_name=project)
-
-        instances_total = 0
-        for region in regions:
-            novaclient = himutils.get_client(Nova, options, logger, region)
-            instances[region] = novaclient.get_project_instances(project_id=project.id)
-            for i in instances[region]:
-                instances_total += 1
-
-        print "Project: %s  [%d instances]" % (project.name, instances_total)
-        print "---------------------------------------------------------------------------"
-        print "  ID:      %s" % project.id
-        print "  Type:    %s" % project_type
-        print "  Created: %s" % project.createdate
-        print "  Admin:   %s" % project_admin
-
-        if len(roles) > 0:
-            print "  Users: "
-            #printer.output_dict({'header': 'Roles in project %s' % project})
-            for role in roles:
-                printer.output_dict(role, sort=True, one_line=True)
-
-        print "\n                 ".join(textwrap.wrap("  Description: " + project.description, 60))
-
-        if instances_total > 0:
-            print "  Instances: "
-            for region in regions:
-                for i in instances[region]:
-                    print "             %s  %s  %s" % (i.id, region, i.name)
-        print
-        count += 1
-
-    printer.output_dict({'header': 'Project list count', 'count': count})
-
 def action_list():
     search_filter = dict()
     if options.filter and options.filter != 'all':
         search_filter['type'] = options.filter
     projects = ksclient.get_projects(**search_filter)
     instances = dict()
-    gc = utils.get_client(Glance, options, logger)
+    volumes = dict()
+
+    # Initiate Designate object
+    dc = himutils.get_client(Designate, options, logger)
+
+    # Project counter
     count = 0
+
+    # Loop through projects
     for project in projects:
         project_type = project.type if hasattr(project, 'type') else '(unknown)'
         project_admin = project.admin if hasattr(project, 'admin') else '(unknown)'
         project_created = project.createdate if hasattr(project, 'createdate') else '(unknown)'
-        project_roles = ksclient.list_roles(project_name=project)
+        project_roles = ksclient.list_roles(project_name=project.name)
 
+        # Get Zones
+        zones = dc.list_project_zones(project.id)
+
+        # Counters
         instances_total = 0
+        volumes_total = 0
+
+        # Get Instances
         for region in regions:
-            novaclient = himutils.get_client(Nova, options, logger, region)
-            instances[region] = novaclient.get_project_instances(project_id=project.id)
+            # Initiate Nova object
+            nc = himutils.get_client(Nova, options, logger, region)
+
+            # Get a list of instances in project
+            instances[region] = nc.get_project_instances(project_id=project.id)
             for i in instances[region]:
                 instances_total += 1
 
-        x = PrettyTable()
-        x.field_names = ['id', 'name', 'region', 'flavor', 'image (status)']
-        x.align['id'] = 'l'
-        x.align['name'] = 'l'
+        # Get Volumes
+        for region in regions:
+            # Initiate Cinder object
+            cc = himutils.get_client(Cinder, options, logger)
 
-        y = PrettyTable()
-        y.field_names = ['meta','value']
-        x.align['meta'] = 'r'
-        x.align['value'] = 'l'
+            # Get a list of volumes in project
+            volumes[region] = cc.get_volumes(detailed=True, search_opts={'project_id': project.id})
+            for i in volumes[region]:
+                volumes_total += 1
 
-        print "PROJECT: %s  [%d instances]" % (project.name, instances_total)
+        # Print header for project
+        print "PROJECT: %s" % project.name
         print '=' * 80
 
-        print "  Metadata:"
-        y.add_row(['ID:', project.id])
-        y.add_row(['Admin:', project_admin])
-        y.add_row(['Type:', project_type])
-        y.add_row(['Created:', project_created])
-        print(y)
-
-        print "  ID:    %s" % project.id
-        print "  Admin: %s" % project_admin
-        print "  Type:  %s" % project_type
-
+        # Print project metadata
+        table_metadata = PrettyTable()
+        table_metadata.border = 0
+        table_metadata.header = 0
+        table_metadata.field_names = ['meta','value']
+        table_metadata.align['meta'] = 'r'
+        table_metadata.align['value'] = 'l'
+        table_metadata.add_row(['ID:', project.id])
+        table_metadata.add_row(['Admin:', project_admin])
+        table_metadata.add_row(['Type:', project_type])
+        table_metadata.add_row(['Created:', project_created])
+        table_metadata.add_row(['Description:', project.description])
         if len(project_roles) > 0:
-            print "  Users: "
-            #printer.output_dict({'header': 'Roles in project %s' % project})
+            users = []
             for role in project_roles:
-                #printer.output_dict(role, sort=True, one_line=True)
                 user = role['group'].replace('-group', '')
-                print "    %s" % user
-                #print role.group
+                users.append(user)
+            table_metadata.add_row(['Users:', "\n".join(users)])
+        print(table_metadata)
 
-        print "  Created: %s" % project_created
-        print "\n               ".join(textwrap.wrap("  Description: " + project.description, 60))
-        if instances_total > 0:
-            print "  Instances: "
+        # Print Zones table
+        if len(zones) > 0:
+            table_zones = PrettyTable()
+            table_zones.field_names = ['id', 'name']
+            table_zones.align['id'] = 'l'
+            table_zones.align['name'] = 'l'
+            for zone in zones:
+                table_zones.add_row([zone['id'], zone['name']])
+            print "\n  Zones (%d): " % len(zones)
+            print(table_zones)
+
+        # Print Volumes table
+        if volumes_total > 0:
+            table_volumes = PrettyTable()
+            table_volumes.field_names = ['id', 'size', 'region']
+            table_volumes.align['id'] = 'l'
+            table_volumes.align['size'] = 'r'
+            table_volumes.align['region'] = 'l'
             for region in regions:
+                for volume in volumes[region]:
+                    table_volumes.add_row([volume.id, "%d GiB" % volume.size, region])
+            print "\n  Volumes (%d): " % volumes_total
+            print(table_volumes)
+
+        # Print Instances table
+        if instances_total > 0:
+            table_instances = PrettyTable()
+            table_instances.field_names = ['id', 'name', 'region', 'flavor', 'image (status)']
+            table_instances.align['id'] = 'l'
+            table_instances.align['name'] = 'l'
+            table_instances.align['region'] = 'l'
+            table_instances.align['flavor'] = 'l'
+            table_instances.align['image (status)'] = 'l'
+            for region in regions:
+                # Initiate Glance object
+                gc = himutils.get_client(Glance, options, logger)
                 for i in instances[region]:
                     filters = {'id': i.image['id']}
                     image = gc.find_image(filters=filters, limit=1)
@@ -152,21 +147,21 @@ def action_list():
                     else:
                         image_name = '(unknown)'
                         image_status = '(unknown)'
+                    row = []
+                    row.append(i.id)
+                    row.append(i.name)
+                    row.append(region)
+                    row.append(i.flavor["original_name"])
+                    row.append("%s (%s)" % (image_name, image_status))
+                    table_instances.add_row(row)
+            print "\n  Instances (%d): " % instances_total
+            print(table_instances)
 
-                    array = []
-                    array.append(i.id)
-                    array.append(i.name)
-                    array.append(region)
-                    array.append(i.flavor["original_name"])
-                    array.append("%s (%s)" % (image_name, image_status))
-                    x.add_row(array)
-                    #print "             %s  %s %s %s/%s %s" % (i.id, region, i.flavor["original_name"], image_name, image_status, i.name)
-
-            print(x)
-        print
-
+        # Print some vertical space and increase project counter
+        print "\n\n"
         count += 1
 
+    # Finally print out number of projects
     printer.output_dict({'header': 'Project list count', 'count': count})
 
 
