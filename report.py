9a1eaea5e39b43fb4669c104a2ad118bbde25146
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2021-06-19 12:33:46 +0200

    foo

--- report.py
+++ report.py
@@ -299,8 +299,8 @@ def __print_instances(project):
             gc = himutils.get_client(Glance, options, logger)
             for i in instances[region]:
                 if 'id' not in i.image:
-                    image_name = '(unknown)'
-                    image_status = '(unknown)'
+                    image_name = 'N/A'
+                    image_status = 'N/A'
                 else:
                     filters = {'id': i.image['id']}
                     image = gc.find_image(filters=filters, limit=1)
@@ -308,8 +308,8 @@ def __print_instances(project):
                         image_name = image[0]['name']
                         image_status = image[0]['status']
                     else:
-                        image_name = '(unknown)'
-                        image_status = '(unknown)'
+                        image_name = 'N/A'
+                        image_status = 'N/A'
                 row = []
                 row.append(i.id)
                 row.append(i.name)
