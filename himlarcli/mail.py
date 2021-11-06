db44125fc83b9475894d5886d2eb3fdd4e61afe8
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2021-11-06 18:21:05 +0100

    foo

--- himlarcli/mail.py
+++ himlarcli/mail.py
@@ -3,6 +3,8 @@ import ConfigParser
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
 
 class Mail(Client):
 
