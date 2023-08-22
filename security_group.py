21482997103b481fb52c841bf5772bf82f93e534
Trond Hasle Amundsen <trondham@uio.no>
2023-08-22 11:14:57 +0200

    foo

--- security_group.py
+++ security_group.py
@@ -353,8 +353,8 @@ def real_ip_for_netmask(ip, mask):
 
 # Check if security group rule is in use
 def rule_in_use(rule, neutron, nova):
-    instances = nova.get_project_instances(sec_group['project_id'])
     sec_group = neutron.get_security_group(rule['security_group_id'])
+    instances = nova.get_project_instances(sec_group['project_id'])
     if rule['project_id'] != sec_group['project_id']:
         verbose_error(f"Security group project {secgroup['project_id']} != rule project {project.id}")
         return False
