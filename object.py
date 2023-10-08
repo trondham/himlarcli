08c7f6a6727b611026738acd906b84964b5fccd3
Trond Hasle Amundsen <trondham@uio.no>
2023-10-09 00:22:58 +0200

    foo

--- object.py
+++ object.py
@@ -34,18 +34,22 @@ def action_grant():
         himutils.fatal(f'Project not found: {options.project}')
 
     # Get all users from project
-    users = ksclient.list_roles(project_name=options.project)
+    project_users = ksclient.list_roles(project_name=options.project)
+
+    # If option '-u' is specified, limit to the specified
+    # users. Otherwise grant access to all project users
+    if options.users:
+        users = [x for x in project_users if x['group'].replace('-group', '') in options.users]
+    else:
+        users = project_users
 
     # Grant object role for all users
     for user in users:
-        if options.users and user not in options.users:
-            continue
-        print(user)
-#        rc = ksclient.grant_role(email=user['group'], project_name=options.project, role_name='object')
-#        if rc == ksclient.ReturnCode.OK:
-#            himutils.info(f"Granted object access in {options.project} to {user.group}")
-#        elif rc == ksclient.ReturnCode.ALREADY_MEMBER:
-#            himutils.warning(f"User {user.group} already has object access in {options.project}")
+        rc = ksclient.grant_role(email=user['group'], project_name=options.project, role_name='object')
+        if rc == ksclient.ReturnCode.OK:
+            himutils.info(f"Granted object access in {options.project} to {user.group}")
+        elif rc == ksclient.ReturnCode.ALREADY_MEMBER:
+            himutils.warning(f"User {user.group} already has object access in {options.project}")
 
 def action_revoke():
     # Get project, make sure it is valid
@@ -54,7 +58,14 @@ def action_revoke():
         himutils.fatal(f'Project not found: {options.project}')
 
     # Get all users from project
-    users = ksclient.list_roles(project_name=options.project)
+    project_users = ksclient.list_roles(project_name=options.project)
+
+    # If option '-u' is specified, limit to the specified
+    # users. Otherwise revoke access to all project users
+    if options.users:
+        users = [x for x in project_users if x['group'].replace('-group', '') in options.users]
+    else:
+        users = project_users
 
     # Revoke object role for all users
     for user in users:
